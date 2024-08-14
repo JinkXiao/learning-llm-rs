99791b75e79f9c2cbba6e878d1b1e9d92f36a734
JinkXiao <jink2wh@163.com>
2024-08-14 22:54:54 +0800

    ok

--- src/params.rs
+++ src/params.rs
@@ -22,14 +22,47 @@ pub struct LLamaParams<T> {
 
 impl LLamaParams<f32> {
     pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Self {
-        todo!("实现从safetensors文件的模型参数加载");
         // let get_tensor: impl Fn(&str) -> Tensor<f32> = |name: &str| {
-        // ...    
+        // ...
         // };
-        
+
         // LLamaParams {
         //     embedding_table: get_tensor(...),
         //     ...
         // }
+
+        for name in safetensor.names() {
+            println!("{}", name)
+        }
+
+        let get_tensor = |name: &str| match safetensor.tensor(name) {
+            Ok(tensor) => {
+                let shape = tensor.shape().to_vec();
+                let data = tensor
+                    .data()
+                    .chunks_exact(4)
+                    .map(|it| f32::from_be_bytes(it.try_into().unwrap()))
+                    .collect();
+                Tensor::<f32>::new(data, &shape)
+            }
+            Err(_) => Tensor::<f32>::default(&Vec::new()),
+        };
+
+        LLamaParams{
+            embedding_table:get_tensor("lm_head.weight"),
+            rms_att_w:(0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.input_layernorm.weight",i))}).collect(),
+            wq:(0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.self_attn.q_proj.weight",i))}).collect(),
+            wk: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.self_attn.k_proj.weight",i))}).collect(),
+            wv: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.self_attn.v_proj.weight",i))}).collect(),
+            wo: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.self_attn.o_proj.weight",i))}).collect(),
+            rms_ffn_w: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.input_layernorm.weight",i))}).collect(),
+            w_up: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.mlp.up_proj.weight",i))}).collect(),
+            w_gate: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.mlp.gate_proj.weight",i))}).collect(),
+            w_down: (0..config.num_hidden_layers).into_iter().map(|i|{get_tensor(&format!("model.layers.{}.mlp.down_proj.weight",i))}).collect(),
+            rms_out_w: get_tensor("model.norm.weight"),
+            lm_head: get_tensor("lm_head.weight"),
+        }
+
+        //todo!("实现从safetensors文件的模型参数加载");
     }
 }
